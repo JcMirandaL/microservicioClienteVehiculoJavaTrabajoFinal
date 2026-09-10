e2d39e40da90aa337c048f28ee45e0210934aa3b
thamy1191 <tamyrose1191@gmail.com>
2026-09-10 14:55:49 -0600

    carpeta validator

---
+++ src/main/java/com/taller/ms_clientes_vehiculos/common/exception/CedulaDuplicada.java
@@ -0,0 +1,4 @@
+package com.taller.ms_clientes_vehiculos.common.exception;
+
+public class CedulaDuplicada {
+}
---
+++ src/main/java/com/taller/ms_clientes_vehiculos/validator/ClienteValidator.java
@@ -0,0 +1,4 @@
+package com.taller.ms_clientes_vehiculos.validator;
+
+public class ClienteValidator {
+}
