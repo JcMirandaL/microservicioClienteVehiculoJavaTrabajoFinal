b67f4c1126943e5014ff335e723e39878f49e889
thamy1191 <tamyrose1191@gmail.com>
2026-09-16 08:21:05 -0600

    crud

---
+++ src/main/java/com/taller/ms_clientes_vehiculos/mapper/ClienteMapper.java
@@ -0,0 +1,4 @@
+package com.taller.ms_clientes_vehiculos.mapper;
+
+public class ClienteMapper {
+}
---
+++ src/main/java/com/taller/ms_clientes_vehiculos/service/ClienteService.java
@@ -0,0 +1,4 @@
+package com.taller.ms_clientes_vehiculos.service;
+
+public class ClienteService {
+}
